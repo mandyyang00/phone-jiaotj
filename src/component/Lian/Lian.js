57e8ae6a64bb575e462c87da6e604ca87710eaf2
mandyyang00 <659890208@qq.com>
2017-09-08 16:35:36 +0800

    lian

--- src/component/Lian/Lian.js
+++ src/component/Lian/Lian.js
@@ -5,6 +5,11 @@ import {Link} from 'react-router-dom'
 import axios from 'axios'
 import {url} from '../config'
 import {message} from 'antd'
+import classnames from 'classnames'
+import { Select } from 'antd';
+import { DatePicker } from 'antd';
+const Option = Select.Option;
+const { MonthPicker, RangePicker } = DatePicker
 
 
 
@@ -14,7 +19,8 @@ class Lianan extends React.Component{
 		this.state={
 			data:null,
 			visibal:false,
-			lianId:null
+			lianId:null,
+			come:''
 		}
 	}
 	componentDidMount(){
@@ -39,8 +45,8 @@ class Lianan extends React.Component{
 		let code1=document.querySelector('.code1').value
 		let code2=document.querySelector('.code2').value
 		let code3=document.querySelector('.code3').value
-		let come=document.querySelector('.come').value
-		let time1=document.querySelector('.time1').value
+		let come=this.state.come
+		let time1=document.querySelector('.time1 ant-input').value
 		let basic=document.querySelector('.basic').value
 		let yiju=document.querySelector('.yiju').value
 		let about=document.querySelector('.about').value
@@ -62,13 +68,11 @@ class Lianan extends React.Component{
 
 
 		}
-		if(code1.length!==0 && code2.length!==0 && code3.length!==0  && come.length!==0 && time1.length!==0 &&  basic.length!==0 && yiju.length!==0 && code1.trim()!=='' && code2.trim()!=='' && code3.trim()!==''  && come.trim()!=='' && time1.trim()!==''  && basic.trim()!=='' && yiju.trim()!==''  ){
+		
 			axios.post(`${url}/FilingService/Add`,data1)
 				.then(res=>this.handleGo(res))
 				.catch(err=>console.log(err))
-		}else{
-			message.error('资料不完整')
-		}
+		
 	}
 
 	handleGo(res){
@@ -77,6 +81,13 @@ class Lianan extends React.Component{
 			visibal:true
 		})
 	}
+
+
+	handleChange1(value){							
+   	this.setState({
+			seconds:`${value}`,
+			visibal:true
+		})
 	render(){
 			let {data,visibal}=this.state
 		return(
@@ -84,6 +95,7 @@ class Lianan extends React.Component{
 				<Header></Header>	
 				<h1>立案审批表</h1>
 				<Link to='/xunwenbilu'><button>返回上一级</button></Link>
+				 
 				<div className='title'>
 
 					<input type="text" className='code1'/>
@@ -93,88 +105,115 @@ class Lianan extends React.Component{
 					<input type="text" className='code3'/>
 					<span>号</span>
 				</div>
-				{!data ? '加载中' : 
+				{!data ? '加载中' :
 				<table>
 					<tr>
 						<td style={{'width':'40px'}}>案件<br/>来源</td>
-						<td style={{'width':'35%'}}>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='come'/>
+						<td colSpan='3'>
+							<Select  style={{'width':'100%','border':'0','outline':'0'}} className='come' value={this.state.seconds} onChange={this.handleChange1.bind(this)}>
+						      <Option value="1" >1</Option>
+						      <Option value="2">2</Option>
+						      <Option value="3" >3</Option>
+						      <Option value="4">4</Option>
+						  </Select>
 						</td>
+					</tr>
+
+					<tr >
 						<td style={{'width':'40px'}}>受案<br/>时间</td>
-						<td>
+						<td colSpan='3'>
+							<DatePicker style={{'width':'99%','border':'0','outline':'0'}} className='time1'/>
+						</td>
+					</tr>
+					<tr>
+						<td>案由</td>
+						<td colSpan='3'>
 							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='time1'/>
 						</td>
 					</tr>
+
 					<tr>
-						<td colSpan='4'>案件当事人基本情况
+						<td rowSpan='10'>案件当事人基本情况
 						</td>
 					</tr>
+
 					<tr>
-						<td colSpan='2'>公民</td>
-						<td colSpan='2'>法人或其他组织</td>
-					</tr>
-					<tr>
+						<td rowSpan='6'>公民</td>
 						<td>姓名</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
-						</td>
-						<td>名称</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='name3'/>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.InvolvedName}/>
 						</td>
 					</tr>
+						
 					<tr>
 						<td>性别</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
-						</td>
-						<td>地址</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='address1'/>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.InvolvedSex}/>
 						</td>
 					</tr>
+
 					<tr>
 						<td>年龄</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.InvolvedName}/>
 						</td>
-						<td>法定<br/>代表人</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='faren'/>
+					</tr>
+
+					<tr>
+						<td>住址</td>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.InvolvedAddress}/>
 						</td>
 					</tr>
 					<tr>
 						<td>身份<br/>证号</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
-						</td>
-						<td>联系<br/>电话</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='telnumber'/>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.InvolvedIDCode}/>
 						</td>
 					</tr>
 					<tr>
-						<td>住址</td>
-						<td>
-							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
+						<td>联系<br/>电话</td>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='telnumber' value={data.InvolvedTelNo}/>
 						</td>
-						<td></td>
-						<td></td>
+					</tr>
+
+					<tr>
+						<td rowSpan='4'>法人或其他组织</td>
+					</tr>
+
+					<tr>
+						<td>名称</td>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='name3'/>
+						</td>
+					</tr>
+					<tr>
+						<td>法定<br/>代表人</td>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='faren'/>
+						</td>
+					</tr>
+					<tr>
+						<td>地址</td>
+						<td colSpan='2'>
+							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} className='address1'/>
+						</td>
 					</tr>
 					<tr>
 						<td>联系<br/>电话</td>
-						<td>
+						<td colSpan='2'>
 							<input type="text" style={{'width':'90%','border':'0','outline':'0'}} value={data.Name}/>
 						</td>
-						<td></td>
-						<td>
-							
-						</td>
 					</tr>
+
+					
+
 					<tr>
 						<td colSpan='4'>案件基本情况</td>
 						
 					</tr>
+
 					<tr>
 						<td colSpan='4'>
 							<textarea name="" id="" cols="30" rows='4'
@@ -182,10 +221,12 @@ class Lianan extends React.Component{
 							</textarea>
 						</td>
 					</tr>
+
 					<tr>
 						<td colSpan='4'>立案依据</td>
 						
 					</tr>
+
 					<tr>
 						<td colSpan='4'>
 							<textarea name="" id="" cols="30" rows='4'
@@ -193,38 +234,57 @@ class Lianan extends React.Component{
 							</textarea>
 						</td>
 					</tr>
+
 					<tr>
 						<td colSpan='4'>受案机构意见</td>
 						
 					</tr>
+
 					<tr>
 						<td colSpan='4'>
 							<textarea name="" id="" cols="30" rows='4'
 									 style={{'width':'90%','border':'0','outline':'0'}} className='shouanyijian' readOnly='readonly'>
 							</textarea>
 							<p style={{'textIndent':'50%','lineHeight':'20px'}}>签名:
-								<input type="text"  style={{'width':'100px','border':'0','outline':'0'}} className='name1'readOnly='readonly'/>
+								
+								<Select  style={{'width':'100%','border':'0','outline':'0'}} className='name1' value={this.state.seconds} onChange={this.handleChange1.bind(this)}>
+						      <Option value="1" >1</Option>
+						      <Option value="2">2</Option>
+						      <Option value="3" >3</Option>
+						      <Option value="4">4</Option>
+						  </Select>
 							</p>
 							<p style={{'textIndent':'50%','lineHeight':'20px'}}>时间:
-								<input type="text"  style={{'width':'100px','border':'0','outline':'0'}} className='time2'readOnly='readonly'/>
+								
+								<DatePicker style={{'width':'99%','border':'0','outline':'0'}} className='time2'/>
 							</p>
 						</td>
 					</tr>
+
 					<tr>
 						<td colSpan='4'>负责人审批意见</td>
 						
 					</tr>
+					<tr>
 						<td colSpan='4'>
 							<textarea name="" id="" cols="30" rows='4'
 									 style={{'width':'90%','border':'0','outline':'0'}}  className='fuzerenyijian' readOnly='readonly'>
 							</textarea>
 							<p style={{'textIndent':'50%','lineHeight':'20px'}}>签名:
-								<input type="text"  style={{'width':'100px','border':'0','outline':'0'}} className='name2' readOnly='readonly'/>
+								
+								<Select  style={{'width':'100%','border':'0','outline':'0'}} className='name2' value={this.state.seconds} onChange={this.handleChange1.bind(this)}>
+						      <Option value="1" >1</Option>
+						      <Option value="2">2</Option>
+						      <Option value="3" >3</Option>
+						      <Option value="4">4</Option>
+						  	</Select>
 							</p>
 							<p style={{'textIndent':'50%','lineHeight':'20px'}}>时间:
-								<input type="text"  style={{'width':'100px','border':'0','outline':'0'}} className='time3' readOnly='readonly'/>
+								
+								<DatePicker style={{'width':'99%','border':'0','outline':'0'}} className='time3'/>
 							</p>
 						</td>
+					</tr>
 					<tr>
 						<td>备注</td>
 						<td colSpan='3'>
@@ -232,11 +292,9 @@ class Lianan extends React.Component{
 						</td>
 					</tr>
 				</table>}
-				{visibal ? 
+				
 				<Link to='/list'><button onClick={this.handleOk.bind(this)}>提交</button></Link>
-				:
-				 <button onClick={this.handleOk.bind(this)}>提交</button>
-				}
+				
 				
 				
 				
